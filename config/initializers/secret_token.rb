80cd82f4b92e73795797ff1dad4538839ad1c60e
Louis Rose <louis@cs.york.ac.uk>
2011-08-24 10:43:50 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heatspots::Application.config.secret_token = '7d375f2107e9a53faef4b2f5100d231fcb1789a05dceb948501ebfca5f82f4a0e5218c04612dbaed35e1951bbd9e4e99025210b90f9ff66b0b6e91fc16b47571'
