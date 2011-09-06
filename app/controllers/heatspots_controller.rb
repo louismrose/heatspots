1735780373661914e8a47511c4ff43c17cf73e75
Louis Rose <louis@cs.york.ac.uk>
2011-09-06 12:04:01 +0100

    Switched to respond_with form of controller.

--- app/controllers/heatspots_controller.rb
+++ app/controllers/heatspots_controller.rb
@@ -1,6 +1,7 @@
 require 'soap/wsdlDriver'
 
 class HeatspotsController < ApplicationController
+  respond_to :html, :xml, :json
   protect_from_forgery :except => :heat_map_proxy
   
   # GET /heatspots
@@ -9,76 +10,43 @@ class HeatspotsController < ApplicationController
     @heatspots = Heatspot.all
     @heatmap_proxy_url = "http://#{request.host}:#{request.port}/heat_map_proxy"
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @heatspots }
-      format.json { render :json => @heatspots }
-    end
+    respond_with(@heatspots)
   end
 
   # GET /heatspots/1
   # GET /heatspots/1.xml
   def show
     @heatspot = Heatspot.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @heatspot }
-      format.json { render :json => @heatspot }
-    end
+    respond_with(@heatspot)
   end
 
   # GET /heatspots/new
   # GET /heatspots/new.xml
   def new
     @heatspot = Heatspot.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @heatspot }
-      format.json { render :json => @heatspot }
-    end
+    respond_with(@heatspot)
   end
 
   # GET /heatspots/1/edit
   def edit
     @heatspot = Heatspot.find(params[:id])
+    respond_with(@heatspot)
   end
 
   # POST /heatspots
   # POST /heatspots.xml
   def create
     @heatspot = Heatspot.new(params[:heatspot])
-
-    respond_to do |format|
-      if @heatspot.save
-        format.html { redirect_to(@heatspot, :notice => 'Heatspot was successfully created.') }
-        format.xml  { render :xml => @heatspot, :status => :created, :location => @heatspot }
-        format.json { render :json => @heatspot, :status => :created, :location => @heatspot }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @heatspot.errors, :status => :unprocessable_entity }
-        format.json { render :json => @heatspot.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Heatspot was successfully created.' if @heatspot.save
+    respond_with(@heatspot)
   end
 
   # PUT /heatspots/1
   # PUT /heatspots/1.xml
   def update
     @heatspot = Heatspot.find(params[:id])
-
-    respond_to do |format|
-      if @heatspot.update_attributes(params[:heatspot])
-        format.html { redirect_to(@heatspot, :notice => 'Heatspot was successfully updated.') }
-        format.xml  { head :ok }
-        format.json { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @heatspot.errors, :status => :unprocessable_entity }
-        format.json { render :json => @heatspot.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Heatspot was successfully updated.' if @heatspot.update_attributes(params[:heatspot])
+    respond_with(@heatspot)
   end
 
   # DELETE /heatspots/1
@@ -86,12 +54,7 @@ class HeatspotsController < ApplicationController
   def destroy
     @heatspot = Heatspot.find(params[:id])
     @heatspot.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(heatspots_url) }
-      format.xml  { head :ok }
-      format.json { head :ok }
-    end
+    respond_with(@heatspot)
   end
    
   def heat_map_proxy
